ceb419dd2bfc0890caef7a6557d937f3e821c432
BillStephens2022 <two4onebill@yahoo.com>
2024-01-26 06:55:22 -0500

    initial commit JavaFX application

---
+++ src/main/java/com/billstephens/contactsjavafx/HelloController.java
@@ -0,0 +1,14 @@
+package com.billstephens.contactsjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
